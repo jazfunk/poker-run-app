b2bd6729cff3cf1a70ab6bab10ecb768a92ac688
Jeff King <jazfunk@gmail.com>
2020-08-31 21:07:45 -0400

    Revert procfile

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const path = require('path')
-const db = require('./queries')
-const { port } = require('./config')
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const path = require("path");
+const db = require("./queries");
+const { port } = require("./config");
 // Todo: Install Morgan
 
 app.use(express.static(__dirname + "/client/build"));
@@ -16,18 +16,18 @@ app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-)
+);
 
-app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
+app.get("/users", db.getUsers);
+app.get("/users/:id", db.getUserById);
+app.post("/users", db.createUser);
+app.put("/users/:id", db.updateUser);
+app.delete("/users/:id", db.deleteUser);
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 app.listen(port, () => {
-  console.log(`Application Server is running on port ${port}.`)
-})
+  console.log(`Application Server is running on port ${port}.`);
+});
---
+++ validation.js
@@ -0,0 +1,59 @@
+// import env from './env';
+
+/**
+   * isValidEmail helper method
+   * @param {string} email
+   * @returns {Boolean} True or False
+   */
+const isValidEmail = (email) => {
+  const regEx = /\S+@\S+\.\S+/;
+  return regEx.test(email);
+};
+
+/**
+   * validatePassword helper method
+   * @param {string} password
+   * @returns {Boolean} True or False
+   */
+const validatePassword = (password) => {
+  if (password.length <= 5 || password === '') {
+    return false;
+  } return true;
+};
+/**
+   * isEmpty helper method
+   * @param {string, integer} input
+   * @returns {Boolean} True or False
+   */
+const isEmpty = (input) => {
+  if (input === undefined || input === '') {
+    return true;
+  }
+  if (input.replace(/\s/g, '').length) {
+    return false;
+  } return true;
+};
+
+/**
+   * empty helper method
+   * @param {string, integer} input
+   * @returns {Boolean} True or False
+   */
+const empty = (input) => {
+  if (input === undefined || input === '') {
+    return true;
+  }
+};
+
+export {
+  isValidEmail,
+  validatePassword,
+  isEmpty,
+  empty
+};
+
+
+// isValidEmail() method uses regex(Regular Expression) in validating user's provided email address.
+// Check this guide to learn more about Regex
+// validatePassword — This method checks if the length of the password provided by a user at the time of signup or signin is less than or equal to 5 characters.
+// isEmpty method checks if any input provided by a user is undefined or empty.
