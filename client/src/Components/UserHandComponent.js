37c9add9edecd8b69acb21acf93257fedca38cd0
Jeff King <jazfunk@gmail.com>
2020-09-03 23:04:44 -0400

    Adds graphic display of cards from users hands

--- client/src/Components/UserHandComponent.js
+++ client/src/Components/UserHandComponent.js
@@ -3,23 +3,19 @@ import Card from "react-bootstrap/Card";
 import logo from "../Images/PokerRunKingLOGO_NEW.png";
 
 let fullName = "";
-let imagePath = "";
 
 const RunUserHands = (props) => {
   let handsRows = props.hands.map((hand, index) => {
     fullName = `${hand.first_name} ${hand.last_name}` 
-    imagePath = `../Images/${hand.card_suit}.png`
-    // console.log(imagePath);
     return (
-      <Card className ="app-home" key={index}>
-        <Card.Img variant="top" className="img-fluid" src={imagePath} alt="" />
+      <Card className ="hands-cards" key={index}>
+        <Card.Img className="hands-img card-glow" variant="top" src={require(`../Images/${hand.card_suit}.png`)} alt="" />
         <Card.Body>
-          <Card.Title>
+          <Card.Title className="card-face">
             {hand.card_face}
-            {hand.card_suit}
           </Card.Title>
-          <Card.Text>
-            #{hand.hand_id}-{hand.hand_number}-{hand.first_name}
+          <Card.Text className="hands-number">
+            {hand.hand_number}-{hand.hand_id}
           </Card.Text>
         </Card.Body>
       </Card>
@@ -39,7 +35,7 @@ const RunUserHands = (props) => {
       <Card>
         <Card.Img variant="top" src={logo} />
         <Card.Body>
-          <Card.Title>No Cards</Card.Title>
+          <Card.Title className="card-face">No Cards</Card.Title>
           <Card.Text>No cards found</Card.Text>
         </Card.Body>
       </Card>
